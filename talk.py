14a32506c84f833b671d44a6007c1bdd4ef048d9
EnochDemilade <140633736+EnochDemilade@users.noreply.github.com>
2023-07-31 11:08:33 -0400

    Update talk.py

--- talk.py
+++ talk.py
@@ -1,6 +1,10 @@
+# import text to speech
 import pyttsx3
 
+# create a function
 def talk(text):
+    # initialise a talk engine
     engine = pyttsx3.init()
+    # say what is in the text parameter
     engine.say(text)
     engine.runAndWait()
